ce8d63f77f1e16a9674c7184d05c69702030f65e
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-04-29 21:04:54 -0300

    chore(services): removido services nao utilizados

--- src/app/services/User/CardapioService.js
+++
@@ -1,9 +0,0 @@
-import Cardapio from "@/app/models/Cardapio";
-
-class CardapioService {
-    async create(data) {
-        return Cardapio.create(data);
-    }
-}
-
-export default new CardapioService();
--- src/app/services/User/UserService.js
+++
@@ -1,9 +0,0 @@
-import User from '@/app/models/User';
-
-class UserService {
-  async create(data) {
-    return User.create(data);
-  }
-}
-
-export default new UserService();
