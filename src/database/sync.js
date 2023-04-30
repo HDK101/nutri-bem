5734474e8363c7967ae728377a3f4c1cff60efa8
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-04-30 14:09:52 -0300

    feat(database): faltando sync dos models

--- src/database/sync.js
+++ src/database/sync.js
@@ -1,5 +1,13 @@
+import Food from '@/app/models/Food';
+import Menu from '@/app/models/Menu';
+import Patient from '@/app/models/Patient';
+import Restriction from '@/app/models/Restriction';
 import User from '@/app/models/User';
 
 export default async function sync() {
   await User.sync({ alter: true });
+  await Food.sync({ alter: true });
+  await Restriction.sync({ alter: true });
+  await Menu.sync({ alter: true });
+  await Patient.sync({ alter: true });
 }
