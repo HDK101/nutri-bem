b754da2600693f328f72e1bf4b654941490c7b7b
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-05-06 18:37:25 -0300

    feat: sync dos models

--- src/database/sync.js
+++ src/database/sync.js
@@ -1,13 +1,17 @@
 import Food from '@/app/models/Food';
+import FoodRestriction from '@/app/models/FoodRestriction';
 import Menu from '@/app/models/Menu';
 import Patient from '@/app/models/Patient';
+import PatientRestriction from '@/app/models/PatientRestriction';
 import Restriction from '@/app/models/Restriction';
 import User from '@/app/models/User';
 
 export default async function sync() {
+  await PatientRestriction.sync({ alter: true });
   await User.sync({ alter: true });
   await Food.sync({ alter: true });
   await Restriction.sync({ alter: true });
+  await FoodRestriction.sync({ alter: true });
   await Menu.sync({ alter: true });
   await Patient.sync({ alter: true });
 }
