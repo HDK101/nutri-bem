01700095fcd737e2c127b4254da8fb3197f0e45f
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-05-06 18:27:48 -0300

    feat: schemas de validacao

---
+++ src/app/schemas/createFood.js
@@ -0,0 +1,9 @@
+
+import * as yup from 'yup'
+
+const createFood = yup.object({
+  name: yup.string().required('Campo de nome está vazio'),
+  height: yup.number().integer().positive().typeError('Campo de quantidade deve ser um número').required('Campo de quantidade está vazio'),
+});
+
+export default createFood;
---
+++ src/app/schemas/createPatient.js
@@ -0,0 +1,12 @@
+import * as yup from 'yup'
+
+const createPatient = yup.object({
+  name: yup.string().required('Campo de nome está vazio'),
+  document: yup.string().required('Campo de nome está vazio').matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, 'Não está no formato de CPF'),
+  height: yup.number().integer().positive().typeError('Campo de altura deve ser um número').required('Campo de altura está vazio'),
+  weight: yup.number().integer().positive().typeError('Campo de peso deve ser um número').required('Campo de peso está vazio'),
+  sex: yup.string('Campo de sexo deve ser uma string').required('Campo de sexo está vazio'),
+  historic: yup.string('Campo de histórico deve ser uma string'),
+});
+
+export default createPatient;
