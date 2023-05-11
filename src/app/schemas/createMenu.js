93b8d78784076e029deff46524e83e31abcbc1d9
Caue <cauegastaldi@outlook.com>
2023-05-11 19:29:19 -0300

    fix: changed regex of menu time attribute on schema "createMenu"

--- src/app/schemas/createMenu.js
+++ src/app/schemas/createMenu.js
@@ -2,7 +2,7 @@ import * as yup from 'yup'
 
 const createMenu = yup.object({
   name: yup.string().required('Campo de nome está vazio'),
-  time: yup.string().trim().matches("/\d+:\d+/").typeError("Tempo deve estar no formato HH:MM").required('Campo de tempo está vazio'),
+  time: yup.string().trim().matches("/\d\d:\d\d/").typeError("Tempo deve estar no formato HH:MM").required('Campo de tempo está vazio'),
 });
 
 export default createMenu;
