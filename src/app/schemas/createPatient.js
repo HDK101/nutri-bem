66546cbebadc846d308b8d72bcebeddcb12b1119
Danilo Araujo <x45danilo45x@gmail.com>
2023-05-12 10:58:29 -0300

    fix(schemas): chaging patient gender verification error message

--- src/app/schemas/createPatient.js
+++ src/app/schemas/createPatient.js
@@ -3,11 +3,9 @@ import * as yup from 'yup';
 const createPatient = yup.object({
   name: yup.string().required('Campo de nome está vazio'),
   document: yup.string().required('Campo de nome está vazio').matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, 'Não está no formato de CPF'),
-  height: yup.number().integer().positive().typeError('Campo de altura deve ser um número')
-    .required('Campo de altura está vazio'),
-  weight: yup.number().integer().positive().typeError('Campo de peso deve ser um número')
-    .required('Campo de peso está vazio'),
-  sex: yup.string('Campo de sexo deve ser uma string').oneOf(['M', 'F'], 'Valor deve ser M ou F').required('Campo de sexo está vazio'),
+  height: yup.number().integer().positive().typeError('Campo de altura deve ser um número').required('Campo de altura está vazio'),
+  weight: yup.number().integer().positive().typeError('Campo de peso deve ser um número').required('Campo de peso está vazio'),
+  sex: yup.string('Campo de sexo deve ser uma string').oneOf(['M', 'F'], 'Valor deve ser Feminino(F) ou Masculino(M)').required('Campo de sexo está vazio'),
   historic: yup.string('Campo de histórico deve ser uma string'),
 });
 
