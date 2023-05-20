73211c95af6cb2681a13a2bec38b7eb9d6c8d123
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-05-20 16:53:59 -0300

    fix: esconder header quando nao esta autorizado

--- src/app/middlewares/authorize.js
+++ src/app/middlewares/authorize.js
@@ -1,7 +1,9 @@
 export default async function authorize(ctx, next) {
   const { id } = ctx.session;
 
-  if (typeof id === 'undefined') return ctx.view('unauthorized');
+  if (typeof id === 'undefined') return ctx.view('unauthorized', {
+    hideHeader: true,
+  });
 
   await next();
 }
