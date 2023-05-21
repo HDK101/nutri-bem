d975c205da0fe9dbd8d64d43c02af1d077875264
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-05-20 22:35:46 -0300

    feat: format de cpf no paciente

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+const inputs = document.getElementsByClassName('format-cpf');
+
+const indexes = [...Array(inputs.length).keys()];
+
+function formatCpf(text) {
+  const textNumbers = text.replace(/\D/g, '');
+
+  const firstGroup = textNumbers.slice(0, 3);
+
+  const secondGroup = textNumbers.slice(3, 6);
+
+  const thirdGroup = textNumbers.slice(6, 9);
+
+  const fourthGroup = textNumbers.slice(9, 11);
+
+  if (textNumbers.length <= 3) return firstGroup;
+  if (textNumbers.length > 3 && textNumbers.length <= 6) return `${firstGroup}.${secondGroup}`;
+  if (textNumbers.length > 6 && textNumbers.length <= 9) return `${firstGroup}.${secondGroup}.${thirdGroup}`;
+  if (textNumbers.length > 9) return `${firstGroup}.${secondGroup}.${thirdGroup}-${fourthGroup}`;
+}
+
+indexes.forEach((index) => {
+  const element = inputs.item(index);
+  element.addEventListener('keyup', () => {
+    const formattedCpf = formatCpf(element.value);
+    element.value = formattedCpf;
+  });
+});
